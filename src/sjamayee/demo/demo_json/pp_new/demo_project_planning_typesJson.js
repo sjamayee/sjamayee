344f95cf612a05625a4a2aaf7bf8f92855265537
Jan Creemers <j.creemers@telenet.be>
2011-04-02 14:38:25 +0200

    demo changes Please enter the commit message for your changes. Lines starting 	new file:   src/sjamayee/demo/demo_json/pp_new/demo_project_planning_dataEntitiesJson.js 	new file:   src/sjamayee/demo/demo_json/pp_new/demo_project_planning_dataRelationsJson.js 	new file:   src/sjamayee/demo/demo_json/pp_new/demo_project_planning_modelEntitiesJson.js 	new file:   src/sjamayee/demo/demo_json/pp_new/demo_project_planning_modelRelationsJson.js 	new file:   src/sjamayee/demo/demo_json/pp_new/demo_project_planning_typesJson.js 	new file:   src/sjamayee/demo/demo_json/pp_old/demoFiles.html 	new file:   src/sjamayee/demo/demo_json/pp_old/demo_project_planning_dataEntitiesJson.js 	new file:   src/sjamayee/demo/demo_json/pp_old/demo_project_planning_dataRelationsJson.js 	new file:   src/sjamayee/demo/demo_json/pp_old/demo_project_planning_modelEntitiesJson.js 	new file:   src/sjamayee/demo/demo_json/pp_old/demo_project_planning_modelRelationsJson.js 	new file:   src/sjamayee/demo/demo_json/pp_old/demo_project_planning_typesJson.js 	new file:   src/sjamayee/demo/sjamayee.css 	new file:   src/sjamayee/demo/sjamayee_base.js 	new file:   src/sjamayee/demo/sjamayee_demo.tmproj 	new file:   src/sjamayee/demo/sjamayee_grid.js 	new file:   src/sjamayee/demo/sjamayee_pmvc.js 	new file:   src/sjamayee/demo/text_bg_ruler91.gif 	modified:   src/sjamayee/html/sjamayee.html

---
+++ src/sjamayee/demo/demo_json/pp_new/demo_project_planning_typesJson.js
@@ -0,0 +1,11 @@
+loadTypes({"types":[
+{"id":"1","type":"Project","code":"PROJ","name":"Project","ver":"0","description":"Project definition.","in_use":"Y","object":"sja_project_planning_projects"},
+{"id":"2","type":"Resource","code":"RSRC","name":"Resource","ver":"0","description":"Resource definition.","in_use":"Y","object":"sja_project_planning_resources"},
+{"id":"3","type":"ProjectResource","code":"PRRS","name":"Project Resource","ver":"0","description":"Project Resource linkage.","in_use":"Y","object":"sja_project_planning_projects_resources"},
+{"id":"4","type":"ResourceUsage","code":"RUSG","name":"Resource Usage","ver":"0","description":"Resource usage definition.","in_use":"Y","object":"sja_project_planning_resource_usage"},
+{"id":"5","type":"ResourceType","code":"RTYP","name":"Resource Type","ver":"0","description":"Resource type definition.","in_use":"Y","object":"sja_project_planning_resource_types"},
+{"id":"6","type":"CalendarDate","code":"CDAT","name":"Calendar Date","ver":"0","description":"Calendar date definition.","in_use":"Y","object":"sja_project_planning_calendar"},
+{"id":"9999999907","type":"Amount","code":"AMNT","name":"Amount","ver":"0","description":"Amount.","in_use":"Y","object":""},
+{"id":"9999999908","type":"Url","code":"URL","name":"Unified Resource Locator","ver":"0","description":"Unified Resource Locator.","in_use":"Y","object":""},
+{"id":"9999999909","type":"Object","code":"OBJ","name":"Object","ver":"0","description":"Object","in_use":"Y","object":""},
+]});
---
+++ src/sjamayee/demo/demo_json/pp_old/demo_project_planning_modelEntitiesJson.js
@@ -0,0 +1,8 @@
+loadModelEntities({"entities":[
+{"id":"1","name":"Calendar Date","version":"1","description":"Calendar date definition.","type_id":"6","text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"2","name":"Resource type","version":"1","description":"Resource type definition.","type_id":"5","text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"3","name":"Project","version":"1","description":"Project definition.","type_id":"1","text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"4","name":"Resource","version":"1","description":"Resource definition.","type_id":"2","text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"5","name":"Project Resource","version":"1","description":"Project Resource linkage","type_id":"3","text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"6","name":"Resource usage","version":"1","description":"Resource usage definition.","type_id":"4","text_id":null,"cby_id":"1","mby_id":"1"}
+]});
