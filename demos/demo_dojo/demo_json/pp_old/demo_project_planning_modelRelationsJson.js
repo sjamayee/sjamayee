424b65fc606db1cf7323ce0cff2197a0482e7251
Jan Creemers <j.creemers@telenet.be>
2011-04-15 15:05:50 +0200

    reordered files for demos. 	new file:   demos/demo1/hgrabber.gif 	new file:   demos/demo1/sjamayee.css 	new file:   demos/demo1/sjamayee.html 	new file:   demos/demo1/sjamayee_base.js 	new file:   demos/demo1/sjamayee_grid.js 	new file:   demos/demo1/sjamayee_pmvc.js 	new file:   demos/demo1/text_bg_ruler91.gif 	new file:   demos/demo1/vgrabber.gif 	new file:   demos/demo2/sjamayee.html 	new file:   demos/demos.html

---
+++ demos/demo_dojo/demo_json/pp_old/demo_project_planning_modelRelationsJson.js
@@ -0,0 +1,8 @@
+loadModelRelations({"relations":[
+{"id":"1","name":"Project_StartDate","version":"1","parent_entity_id":"3","child_entity_id":"1","previous_id":null,"next_id":"2","text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"2","name":"Project_EndDate","version":"1","parent_entity_id":"3","child_entity_id":"1","previous_id":"1","next_id":null,"text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"3","name":"Resource_Type","version":"1","parent_entity_id":"4","child_entity_id":"2","previous_id":null,"next_id":null,"text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"4","name":"ProjectResource_Project","version":"1","parent_entity_id":"5","child_entity_id":"3","previous_id":null,"next_id":"5","text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"5","name":"ProjectResource_Resource","version":"1","parent_entity_id":"5","child_entity_id":"4","previous_id":"4","next_id":null,"text_id":null,"cby_id":"1","mby_id":"1"},
+{"id":"6","name":"ResourceUsage_ProjectResource","version":"1","parent_entity_id":"6","child_entity_id":"5","previous_id":null,"next_id":null,"text_id":null,"cby_id":"1","mby_id":"1"}
+]});
---
+++ demos/demo_dojo/demo_json/pp_old/demo_project_planning_typesJson.js
@@ -0,0 +1,8 @@
+loadTypes({"types":[
+{"id":"1","type":"Project","code":"PROJ","name":"Project","version":"1","description":"Project definition.","in_use":"Y","object":"sja_project_planning_projects","cby_id":"1","mby_id":"1"},
+{"id":"2","type":"Resource","code":"RSRC","name":"Resource","version":"1","description":"Resource definition.","in_use":"Y","object":"sja_project_planning_resources","cby_id":"1","mby_id":"1"},
+{"id":"3","type":"Project_Resource","code":"PRRS","name":"Project Resource","version":"1","description":"Project Resource linkage","in_use":"Y","object":"sja_project_planning_projects_resources","cby_id":"1","mby_id":"1"},
+{"id":"4","type":"Resource Usage","code":"RUSG","name":"Resource usage","version":"1","description":"Resource usage definition.","in_use":"Y","object":"sja_project_planning_resource_usage","cby_id":"1","mby_id":"1"},
+{"id":"5","type":"Resource Type","code":"RTYP","name":"Resource type","version":"1","description":"Resource type definition.","in_use":"Y","object":"sja_project_planning_resource_types","cby_id":"1","mby_id":"1"},
+{"id":"6","type":"Calendar Date","code":"CDAT","name":"Calendar Date","version":"1","description":"Calendar date definition.","in_use":"Y","object":"sja_project_planning_calendar","cby_id":"1","mby_id":"1"}
+]});
